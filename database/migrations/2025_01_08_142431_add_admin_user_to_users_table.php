6961bc33f047fbc4ea6b6b6588e0e55573d1be0b
Anmol Rathi <anmolrathi@Anmols-MacBook-Pro.local>
2025-01-08 09:53:21 -0500

    Adding an admin user to the system through migration file.

---
+++ database/migrations/2025_01_08_142431_add_admin_user_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Omari Atkins',
+            'email' => 'omari.atkins@eazyeats.com',
+            'password' => bcrypt('password123'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('users')->where('role', 'admin')->delete();
+    }
+};
