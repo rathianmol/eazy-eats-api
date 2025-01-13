cf7b22229d5db271cf8f915d1f2c789e11669397
rathianmol <rathianmol25@gmail.com>
2025-01-13 10:39:49 -0500

    Adding the Authenticate middle-ware, as it is being used in the Kernel file currently.

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
